192886e90d9feab388be6c881f3229b8575e6885
Ryan Ferguson <fergusman@gmail.com>
2017-08-07 16:33:45 -0400

    first commit, a little late

---
+++ src/actions/index.js
@@ -0,0 +1,31 @@
+import request from 'superagent'
+
+export const REQUEST_GIFS = 'REQUEST_GIFS'
+export const OPEN_MODAL = 'OPEN_MODAL'
+export const CLOSE_MODAL = "CLOSE_MODAL"
+
+const API_URL = 'http://api.giphy.com/v1/gifs/search?q='
+const API_KEY = '3b22cecb9a24456abb5375c4d7068126'
+
+export function requestGifs(term = null) {
+  const data = request.get(`${API_URL}${term.replace(/\s/g, '+')}&api_key=${API_KEY}`);
+
+  console.log(term)
+  return {
+    type: REQUEST_GIFS,
+    payload: data
+  }
+}
+
+export function openModal(gif) {
+  return {
+    type: OPEN_MODAL,
+    gif
+  }
+}
+
+export function closeModal() {
+  return {
+    type: CLOSE_MODAL
+  }
+}
