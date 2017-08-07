858e8ee5d07ca00dabea807ab7568f91bd8a4e5f
Ryan Ferguson <fergusman@gmail.com>
2017-08-07 18:22:20 -0400

    added auth

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
 import request from 'superagent'
 
-export const REQUEST_GIFS = 'REQUEST_GIFS'
-export const OPEN_MODAL = 'OPEN_MODAL'
-export const CLOSE_MODAL = "CLOSE_MODAL"
+export const REQUEST_GIFS = 'REQUEST_GIFS';
+export const OPEN_MODAL = 'OPEN_MODAL';
+export const CLOSE_MODAL = "CLOSE_MODAL";
+export const SIGN_IN_USER = 'SIGN_IN_USER';
+export const SIGN_OUT_USER = 'SIGN_OUT_USER';
 
 const API_URL = 'http://api.giphy.com/v1/gifs/search?q='
 const API_KEY = '3b22cecb9a24456abb5375c4d7068126'
@@ -28,4 +30,16 @@ export function closeModal() {
   return {
     type: CLOSE_MODAL
   }
+}
+
+export function signInUser() {
+    return {
+        type: SIGN_IN_USER
+    }
+}
+
+export function signOutUser() {
+    return {
+        type: SIGN_OUT_USER
+    }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,16 @@
 import { combineReducers } from 'redux'; 
 import GifsReducer from './gifs'; 
 import ModalReducer from './modal';
+import AuthReducer from './auth';
+import { routerReducer } from 'react-router-redux';
+import { reducer as FormReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
+  auth: AuthReducer,
+  form: FormReducer,
   gifs: GifsReducer,
-  modal: ModalReducer
+  modal: ModalReducer,
+  router: routerReducer,
 }); 
 
 export default rootReducer; 
